3a29dbab11a5fb470eb9297894a1731966a78ed2
Apoorv Parijat <apoorvparijat@gmail.com>
2018-07-10 16:56:23 +0530

    Remove comment

--- src/helpers/umzug-helper.js
+++ src/helpers/umzug-helper.js
@@ -43,7 +43,6 @@ module.exports = {
       options.tableName = this.getTableName(type);
       if (extraOptions.schema) {
         options.schema = extraOptions.schema;
-        // options.schema = 'sps_emya_39';
         console.log('~~~~ getStorageOptions() options.schema =', options.schema);
       }
     }
